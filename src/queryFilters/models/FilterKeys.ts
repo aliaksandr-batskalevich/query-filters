a682b007933dd2d7fd9dbe0220f0234dc272cccf
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-12-15 11:48:47 +0300

    feature: added FilterKeyTypes

--- src/queryFilters/models/FilterKeys.ts
+++ src/queryFilters/models/FilterKeys.ts
@@ -1,8 +1,28 @@
+export enum FilterKeyTypes {
+    SINGLE_STR = "SINGLE_STR",
+    MULTIPLE_STR = "MULTIPLE_STR",
+    SINGLE_NUM = "SINGLE_NUM",
+    MULTIPLE_NUM = "MULTIPLE_NUM",
+    BOOLEAN = "BOOLEAN",
+}
+
 export enum FilterKeys {
     CATEGORY = 'category',
     LIST = 'list',
     TAG = 'tag',
     NAME = 'name',
+    PAGE = 'page',
+    STRICT = 'strict',
 }
+
+export const filterTypes = {
+    [FilterKeys.CATEGORY]: FilterKeyTypes.MULTIPLE_STR,
+    [FilterKeys.LIST]: FilterKeyTypes.SINGLE_STR,
+    [FilterKeys.TAG]: FilterKeyTypes.MULTIPLE_STR,
+    [FilterKeys.NAME]: FilterKeyTypes.SINGLE_STR,
+    [FilterKeys.PAGE]: FilterKeyTypes.SINGLE_NUM,
+    [FilterKeys.STRICT]: FilterKeyTypes.BOOLEAN,
+} as const;
+
 
 export const filterKeys = Object.values(FilterKeys) as FilterKeys[];
